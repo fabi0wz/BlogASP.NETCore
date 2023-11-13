4b5a83fc0e5c417fbad32424905d31ee961c2a61
Fábio <fabiomiguel3.10@gmail.com>
2023-11-13 23:52:13 +0000

    goncalo merge

--- AspNetBlog/Controllers/Post_User_LikesController.cs
+++ AspNetBlog/Controllers/Post_User_LikesController.cs
@@ -7,16 +7,20 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using AspNetBlog.Data;
 using AspNetBlog.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 
 namespace AspNetBlog.Controllers
 {
     public class Post_User_LikesController : Controller
     {
         private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public Post_User_LikesController(ApplicationDbContext context)
+        public Post_User_LikesController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
 
         // GET: Post_User_Likes
@@ -44,25 +48,26 @@ namespace AspNetBlog.Controllers
 
             return View(post_User_Likes);
         }
-
-        // GET: Post_User_Likes/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
-
+        
         // POST: Post_User_Likes/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
+        [Authorize]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Like_Id")] Post_User_Likes post_User_Likes)
+        public async Task<IActionResult> Create([Bind("Post")] Post_User_Likes post_User_Likes)
         {
-            if (ModelState.IsValid)
+            var post = await _context.Post.FindAsync(post_User_Likes.Post.Post_Id);
+            var user = await _userManager.GetUserAsync(HttpContext.User);
+            
+            post_User_Likes.User = user;
+            post_User_Likes.Post = post;
+            
+            if (user != null && post != null)
             {
                 _context.Add(post_User_Likes);
                 await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
+                return RedirectToAction("Details", "Posts", new { id = post_User_Likes.Post.Post_Id});
             }
             return View(post_User_Likes);
         }
@@ -83,76 +88,33 @@ namespace AspNetBlog.Controllers
             return View(post_User_Likes);
         }
 
-        // POST: Post_User_Likes/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Like_Id")] Post_User_Likes post_User_Likes)
-        {
-            if (id != post_User_Likes.Like_Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(post_User_Likes);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!Post_User_LikesExists(post_User_Likes.Like_Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(post_User_Likes);
-        }
-
         // GET: Post_User_Likes/Delete/5
+        [Authorize]
         public async Task<IActionResult> Delete(int? id)
         {
-            if (id == null || _context.Post_User_Likes == null)
-            {
-                return NotFound();
-            }
-
-            var post_User_Likes = await _context.Post_User_Likes
-                .FirstOrDefaultAsync(m => m.Like_Id == id);
-            if (post_User_Likes == null)
-            {
-                return NotFound();
-            }
-
-            return View(post_User_Likes);
+            return RedirectToAction("Details", "Posts", new { id = id});
         }
 
         // POST: Post_User_Likes/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
+        public async Task<IActionResult> DeleteConfirmed(int id, int postId)
         {
             if (_context.Post_User_Likes == null)
             {
                 return Problem("Entity set 'ApplicationDbContext.Post_User_Likes'  is null.");
             }
-            var post_User_Likes = await _context.Post_User_Likes.FindAsync(id);
+            var post = await _context.Post.FindAsync(postId);
+            var user = await _userManager.GetUserAsync(HttpContext.User);
+            var post_User_Likes = await _context.Post_User_Likes
+                .FirstOrDefaultAsync(m => m.User == user && m.Post == post);
             if (post_User_Likes != null)
             {
                 _context.Post_User_Likes.Remove(post_User_Likes);
             }
             
             await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
+            return RedirectToAction("Details", "Posts", new { id = post.Post_Id});
         }
 
         private bool Post_User_LikesExists(int id)
--- AspNetBlog/ViewModels/PostDetailsViewModel.cs
+++ AspNetBlog/ViewModels/PostDetailsViewModel.cs
@@ -8,5 +8,6 @@ namespace AspNetBlog.ViewModels
         public List<Post> Posts { get; set; }
         public List<Post_Images> PostImages { get; set; }
         public List<Post_User_Comments> PostUserComments { get; set; }
+        public List<Post_User_Likes> PostUserLikes { get; set; }
     }
 }
