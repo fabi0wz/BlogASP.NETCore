8d3e9dbaef1c864f7b12b6261b5c0db3993108bf
Goncalo Moreira <goncalo.moreira.t0123177@edu.atec.pt>
2023-11-13 23:33:25 +0000

    BackOffice Implemented/Some UI to

---
+++ AspNetBlog/ViewModels/BackOfficeViewModel.cs
@@ -0,0 +1,12 @@
+﻿using AspNetBlog.Models;
+
+namespace AspNetBlog.ViewModels;
+
+public class BackOfficeViewModel
+{
+    public ApplicationUser User { get; set; }
+    public List<ApplicationUser> Users { get; set; }
+
+}
+
+
